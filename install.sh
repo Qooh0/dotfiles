9b51d282126683277035690cc5d1c6d9bcb7c7d3
Kaieda Kensuke <kaieda.kensuke@qadiff.com>
2025-02-16 09:04:49 +0000

    add gitconfig

--- install.sh
+++ install.sh
@@ -2,10 +2,13 @@
 
 # dotfiles があるディレクトリ
 DOTFILES_DIR="$HOME/dotfiles"
+# dotfiles backup directory
+DOTFILES_BK_DIR="$HOME/.dotfiles_bk"
 
 # リンクを作りたいファイル (必要に応じて増減してください)
 FILES=(
   ".bash_aliases"
+  ".gitconfig"
   ".zshrc"
   ".vimrc"
   ".tmux.conf"
@@ -13,12 +16,13 @@ FILES=(
 )
 
 echo "Linking dotfiles from $DOTFILES_DIR to $HOME"
+mkdir -p $DOTFILES_BK_DIR
 
 for file in "${FILES[@]}"; do
   # もしホームディレクトリにファイル/リンクが既に存在する場合はバックアップしておく
   if [ -e "$HOME/$file" ] && [ ! -L "$HOME/$file" ]; then
-    echo "Found existing file $HOME/$file. Moving it to $HOME/$file.bk"
-    mv "$HOME/$file" "$HOME/$file.bk"
+    echo "Found existing file $HOME/$file. Moving it to $DOTFILES_BK_DIR/$file.bk"
+    mv "$HOME/$file" "$DOTFILES_BK_DIR/$file.bk"
   elif [ -L "$HOME/$file" ]; then
     echo "Removing existing symlink $HOME/$file"
     rm "$HOME/$file"
